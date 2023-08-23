ba8e20e6de1a64471e3719f5b0e462348ddd30d4
xiexieni1997 <1394130864@qq.com>
2023-08-23 10:13:44 +0800

    阿大撒

---
+++ mian.cpp
@@ -0,0 +1,6 @@
+#include <windows.h>
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
+    MessageBox(NULL, "Hello, World11!", "Message", MB_OK);
+    return 0;
+}
